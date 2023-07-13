a079ae7ed4f7daedec32430f8de62ba22bf8463e
SimeonStoyanov01 <simosstoyanov@gmail.com>
2023-07-13 11:58:09 +0300

    init commit

---
+++ src/main/java/com/example/tinqin/zoostorestorage/ZooStoreStorageApplication.java
@@ -0,0 +1,13 @@
+package com.example.tinqin.zoostorestorage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZooStoreStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZooStoreStorageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tinqin/zoostorestorage/ZooStoreStorageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tinqin.zoostorestorage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZooStoreStorageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
